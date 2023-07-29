26ccbe0a5b060d489eabfcdabb4cc588613420ab
Oseib4 <oseibn4@gmail.com>
2023-07-29 19:45:38 +0200

    modified src/split-object/split.js,src/split-object/split.spec.js

--- src/split-object/split.js
+++ src/split-object/split.js
@@ -1,16 +1,5 @@
-export function splitObject(key = [], value = []) {
-  return Object.keys(key).map((key) => {
+export const splitObject = (key = [], value = []) => {
+  return Object.keys(key).map((value) => {
     return { [key]: key[value] };
   });
-}
-
-//({ [value]: obj[key] }));
-
-// export function toSeparate   = {
-//   name:'robs', import { splitObject } from './splitObject';
-
-//   age: 25,
-//   tall: true,
-//   userName: 'sbor' };
-
-// return  toSeparate.entries(obj).map(([key, value]) => ({ key, value }));
+};
--- src/split-object/split.spec.js
+++ src/split-object/split.spec.js
@@ -2,7 +2,7 @@ import { splitObject } from './split.js';
 
 describe('splitObject', () => {
   it('should split an object into an array of key-value pairs', () => {
-    expect(splitObject); //.toEqual();
+    expect(splitObject);
   });
   const originalObject = {
     name: 'John',
@@ -10,13 +10,7 @@ describe('splitObject', () => {
     city: 'New York',
   };
 
-  const result = splitObject(originalObject);
+  //   const result = splitObject(originalObject);
 
   const expected = [{ name: 'John' }, { age: 30 }, { city: 'New York' }];
 });
-
-// describe('README test', () => {
-//     it('', () => {
-//       expect(()).toEqual([]);
-//     });
-//   });
