b20ee9d977e7e3d3998ac3b007df00c7839a8378
Oseib4 <oseibn4@gmail.com>
2023-07-29 18:04:00 +0200

    modified src/sort-numbers/sort.spec.js

--- src/sort-numbers/sort.spec.js
+++ src/sort-numbers/sort.spec.js
@@ -1,12 +1,15 @@
 import { arrayOfNumbers } from './sort.js';
 
 describe('README test', () => {
-  it('should sort an array of number', () => {
-    expect(arrayOfNumbers([1.5, 1, -1.5, 0, -1], [-1.5, -1, 0, 1.5, 1]),
-    ).//toEqual([0]);
-    });
+  expect(arrayOfNumbers([1.5, -1, -1.5, 0, -1], [-1.5, -1, 0, 1.5, 1])).toEqual([-1]);
 });
 
 // it('should sort an array of number', () => {
 //  expect(arrayOfNumbers([1.5, 1, -1.5, 0, -1],[-1.5, -1, 0, 1.5, 1])).toEqual[]);
 // };
+
+//it('should sort an array of number', () => {
+//     expect(arrayOfNumbers([1.5, 1, -1.5, 0, -1], [-1.5, -1, 0, 1.5, 1]),
+//     ).toEqual([0])
+//     });
+// });
